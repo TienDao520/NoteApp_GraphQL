06b259446778dafd16c34a10caa5f39d3f626e30
Tien Dao <tiendao5240@gmail.com>
2023-01-10 13:49:41 -0500

    Added ErrorPage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import UserMenu from '../components/UserMenu';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Typography variant='h4' sx={{ mb: '20px' }}>
+        Note App
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'right', mb: '10px' }}>
+        <UserMenu />
+      </Box>
+    </>
+  );
 }
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, Outlet } from 'react-router-dom';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import AuthProvider from '../context/AuthProvider';
+import ProtectedRoute from './ProtectedRoute';
+import ErrorPage from '../pages/ErrorPage';
 
 const AuthLayout = () => {
   return (
@@ -14,15 +16,25 @@ const AuthLayout = () => {
 export default createBrowserRouter([
   {
     element: <AuthLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         element: <Login />,
         path: '/login',
       },
       {
-        element: <Home />,
-        path: '/',
+        element: <ProtectedRoute />,
+        children: [
+          {
+            element: <Home />,
+            path: '/',
+          },
+        ],
       },
+      // {
+      //   element: <Home />,
+      //   path: '/',
+      // },
     ],
   },
 ]);
