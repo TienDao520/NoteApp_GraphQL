d24c47c529b980af0c6499a4ee5f45b0c663ad23
Tien Dao <tiendao5240@gmail.com>
2023-01-09 20:22:01 -0500

    Basic setup for typeDefs-resolvers of ApolloServer

--- server.mjs
+++ server.mjs
@@ -1,7 +1,10 @@
 import express from 'express';
 import http from 'http';
 import { ApolloServer } from '@apollo/server';
-import { AppoloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
 
 //creates a new Express application
 // use this app constant to set up routes, configure middleware, and start the server.
@@ -12,13 +15,40 @@ const app = express();
  */
 const httpServer = http.createServer(app);
 
-const typeDefs = '';
-const resolvers = {};
+/**
+ * Mutation: For delete update value
+ * Subscription: For realtime update to clientwhen there are any changes from server
+ * typeDefs here is like a document a set of rules name and kind of data
+ */
+const typeDefs = `#graphql
+  type Query {
+    name: String
+  }
 
+`;
+/**Handle and send back to client base on query from client */
+const resolvers = {
+  Query: {
+    name: () => {
+      return 'Tien Dao';
+    },
+  },
+};
+
+// schema
 const server = new ApolloServer({
   typeDefs, //
   resolvers,
-  plugins: [AppoloServerPluginDrainHttpServer({ httpServer })],
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 await server.start(); //await without async func when the file is Javascript module '.mjs'
+
+//Adding middleware
+app.use(cors(), bodyParser.json(), expressMiddleware(server));
+
+await new Promise((resolve, reject) => {
+  httpServer.listen({ port: 4000 }, resolve);
+});
+
+console.log('Server is ready at http://localhost:4000');
