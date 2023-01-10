fc695b99787da7a6b9b1337be75defe8cdabd7a5
Tien Dao <tiendao5240@gmail.com>
2023-01-10 11:09:57 -0500

    Added navigate to home when no uid

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,19 +1,37 @@
 import React, { createContext, useEffect } from 'react';
 import { useState } from 'react';
 import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-//Add context to share the login/ authentication through out the app
-const AuthContext = createContext();
+//Add context to share the login/ authentication through out the app in the context
+export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState({});
 
+  const navigate = useNavigate();
   const auth = getAuth();
 
   useEffect(() => {
     const unsubcribed = auth.onIdTokenChanged((user) => {
       console.log('[From AuthProvider]', { user });
+      //If user.uid is there state user will be change and share to all components in app
+      if (user?.uid) {
+        setUser(user);
+        localStorage.setItem('access_token', user.accessToken);
+        return;
+      }
+
+      //reset user information
+      setUser({});
+      localStorage.clear();
+      navigate('/login');
     });
+
+    return () => {
+      unsubcribed();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth]);
   //Values passed to Provider will be share to children to access
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 
 import { Button, Typography } from '@mui/material';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { AuthContext } from '../context/AuthProvider';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const auth = getAuth();
+  //Get the user object value from the public context
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
 
   const handleLoginWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     const res = await signInWithPopup(auth, provider);
     console.log({ res });
   };
+
+  if (user?.uid) {
+    navigate('/');
+    return;
+  }
+
   return (
     <>
       <Typography variant='h5' sx={{ marginBottom: '10px' }}>
