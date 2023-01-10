143ee85ced47fdb9e52d853edb3f2497a1efbd43
Tien Dao <tiendao5240@gmail.com>
2023-01-10 08:41:18 -0500

    Added material ui and font

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import { Button, Typography } from '@mui/material';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <Typography variant='h5' sx={{ marginBottom: '10px' }}>
+        Welcome to Note Application
+      </Typography>
+      <Button variant='outlined'>Login with Google</Button>
+    </>
+  );
 }
