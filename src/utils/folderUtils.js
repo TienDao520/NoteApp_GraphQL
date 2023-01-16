74be1aef5ce492ba11d1cde189efb9d23844c3ea
Tien Dao <tiendao5240@gmail.com>
2023-01-16 14:09:58 -0500

    Added accessToken request Header

--- src/utils/folderUtils.js
+++ src/utils/folderUtils.js
@@ -1,3 +1,5 @@
+import { graphQLRequest } from './request';
+
 export const foldersLoader = async () => {
   const query = `query Folders {
     folders {
@@ -6,17 +8,7 @@ export const foldersLoader = async () => {
       createAt
     }
   }`;
-  const res = await fetch('http://localhost:4000/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-    }),
-  });
-  const { data } = await res.json();
-  console.log({ data });
+
+  const data = await graphQLRequest({ query });
   return data;
 };
