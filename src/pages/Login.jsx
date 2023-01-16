c8dac5be623185118a6bc686503676c2e3b2dcb4
Tien Dao <tiendao5240@gmail.com>
2023-01-16 17:41:09 -0500

    Add mutation register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ import { AuthContext } from '../context/AuthProvider';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { graphQLRequest } from '../utils/request';
 
 export default function Login() {
   const auth = getAuth();
@@ -16,8 +17,22 @@ export default function Login() {
 
   const handleLoginWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
-    const res = await signInWithPopup(auth, provider);
-    console.log({ res });
+    const {
+      user: { uid, displayName },
+    } = await signInWithPopup(auth, provider);
+    const { data } = await graphQLRequest({
+      query: `mutation register($uid: String!, $name: String!) {
+      register(uid: $uid, name: $name) {
+        uid
+        name
+      }
+    }`,
+      variables: {
+        uid,
+        name: displayName,
+      },
+    });
+    console.log('register', { data });
   };
 
   if (user?.uid) {
