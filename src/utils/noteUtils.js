8de33287683768fd6cec9dfed9ff2caa0dbc2a86
Tien Dao <tiendao5240@gmail.com>
2023-01-16 17:25:09 -0500

    using graphQLRequest for Utils

--- src/utils/noteUtils.js
+++ src/utils/noteUtils.js
@@ -1,3 +1,5 @@
+import { graphQLRequest } from './request';
+
 export const notesLoader = async ({ params: { folderId } }) => {
   console.log(['loader'], { folderId });
   const query = `query Folder($folderId: String) {
@@ -10,23 +12,15 @@ export const notesLoader = async ({ params: { folderId } }) => {
       }
     }
   }`;
-  const res = await fetch('http://localhost:4000/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+
+  const data = await graphQLRequest({
+    query,
+    variables: {
+      //folderId getting from params
+      //same with folderId: folderId in ES6 since key and value are the same
+      folderId,
     },
-    body: JSON.stringify({
-      query,
-      variables: {
-        //folderId getting from params
-        //same with folderId: folderId in ES6 since key and value are the same
-        folderId,
-      },
-    }),
   });
-  const { data } = await res.json();
-  console.log('[Note List]', { data });
   return data;
 };
 
@@ -38,20 +32,14 @@ export const noteLoader = async ({ params: { noteId } }) => {
       content
     }
   }`;
-  const res = await fetch('http://localhost:4000/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+
+  const data = await graphQLRequest({
+    query,
+    variables: {
+      //folderId getting from params
+      //same with folderId: folderId in ES6 since key and value are the same
+      noteId,
     },
-    body: JSON.stringify({
-      query,
-      variables: {
-        noteId,
-      },
-    }),
   });
-  const { data } = await res.json();
-  console.log('[Note List]', { data });
   return data;
 };
