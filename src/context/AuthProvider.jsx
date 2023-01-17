df3354974d8a4f4925d9b8a140226152a7a3778a
Tien Dao <tiendao5240@gmail.com>
2023-01-17 10:19:09 -0500

    Fixed issues related to accessToken is expired

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,12 +2,14 @@ import React, { createContext, useEffect, useState } from 'react';
 
 import { getAuth } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
 //Add context to share the login/ authentication through out the app in the context
 export const AuthContext = createContext();
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   const navigate = useNavigate();
   const auth = getAuth();
@@ -18,11 +20,18 @@ export default function AuthProvider({ children }) {
       //If user.uid is there state user will be change and share to all components in app
       if (user?.uid) {
         setUser(user);
-        localStorage.setItem('accessToken', user.accessToken);
+        //Tracking when accessToken has been changed
+        if (user.accessToken !== localStorage.getItem('accessToken')) {
+          localStorage.setItem('accessToken', user.accessToken);
+          window.location.reload();
+        }
+        setIsLoading(false);
         return;
       }
 
       //reset user information
+      console.log('reset');
+      setIsLoading(false);
       setUser({});
       localStorage.clear();
       navigate('/login');
@@ -36,7 +45,7 @@ export default function AuthProvider({ children }) {
   //Values passed to Provider will be share to children to access
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      {children}
+      {isLoading ? <CircularProgress /> : children}
     </AuthContext.Provider>
   );
 }
