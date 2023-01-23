8bb269cd183f52448ae21e2991b9cf58c228aeed
Tien Dao <tiendao5240@gmail.com>
2023-01-23 14:34:28 -0500

    Changed ws to wss for secure purpose

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_SERVER = 'https://note-app-tiendao-server.onrender.com';
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT =
-  'ws://note-app-tiendao-server.onrender.com/graphql';
+  'wss://note-app-tiendao-server.onrender.com/graphql';
